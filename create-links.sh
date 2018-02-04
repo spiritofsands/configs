6078eb8576ad7165a97556fad533876586372b9a
Kostia Fursenko <spiritofsands@protonmail.com>
2018-02-04 11:36:42 +0200

    create links && profile

---
+++ create-links.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ln -s mozilla-chrome-tweaks mozilla/firefox/*/chrome
+ln -s /home/kos/.config/bash_aliases ~/.bash_aliases
+ln -s /home/kos/.config/bashrc ~/.bashrc
+ln -s /home/kos/.config/coffeelint.json ~/coffeelint.json
+ln -s /home/kos/.config/eslintrc.yml ~/.eslintrc.yml
+ln -s /home/kos/.config/keynavrc ~/.keynavrc
+ln -s /home/kos/.config/mozilla ~/.mozilla
+ln -s /home/kos/.config/redshift.conf ~/.redshift.conf
+ln -s /home/kos/.config/rubocop.yml ~/.rubocop.yml
+ln -s /home/kos/.config/tmux ~/.tmux
+ln -s /home/kos/.config/tmux.conf ~/.tmux.conf
+ln -s /home/kos/.config/toprc ~/.toprc
+ln -s /home/kos/.config/vim ~/.vim
+ln -s /home/kos/.config/vimrc ~/.vimrc
+ln -s /home/kos/.config/Xresources ~/.Xresources
+ln -s ~/.config/profile ~/.profile
+echo 'sudo cp Xresources-xdm /etc/X11/xdm/Xresources'
