6e7daadf394f6961e826def026acd0dac23fb46f
Kostia Fursenko <spiritofsands@protonmail.com>
2018-03-03 13:01:47 +0200

    Add reek config

--- create-links.sh
+++ create-links.sh
@@ -21,6 +21,7 @@ ln -s /home/kos/.config/babelrc /home/kos/.babelrc
 ln -s /home/kos/.config/jshintrc /home/kos/.jshintrc
 ln -s /home/kos/.config/gitignore /home/kos/.gitignore
 git config --global core.excludesfile ~/.gitignore
+ln -s /home/kos/.config/reek /home/kos/.reek
 
 echo
 echo 'sudo cp Xresources-xdm /etc/X11/xdm/Xresources'
