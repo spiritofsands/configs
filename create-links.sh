b4d07a309b56115fea4af7d9f03e519a0a509777
Kostia Fursenko <spiritofsands@protonmail.com>
2019-01-06 15:13:11 +0200

    Add font configuration

--- create-links.sh
+++ create-links.sh
@@ -1,71 +1,129 @@
 #!/bin/bash
 
-links=(
-bash_aliases
-bashrc
-coffeelint.json
-eslintrc.yml
-keynavrc
-mozilla
-redshift.conf
-rubocop.yml
-tmux
-tmux.conf
-toprc
-vim
-vimrc
-profile
-jshintrc
-gitignore
-reek
-clang-format
-reek
-inputrc
-stylelintrc
-Xresources
-moc
-thunderbird
-gtkrc-2.0
-)
+setup_symlinks() {
+    readonly links=(
+    bash_aliases
+    bashrc
+    coffeelint.json
+    eslintrc.yml
+    keynavrc
+    mozilla
+    redshift.conf
+    rubocop.yml
+    tmux
+    tmux.conf
+    toprc
+    vim
+    vimrc
+    profile
+    jshintrc
+    gitignore
+    reek
+    clang-format
+    reek
+    inputrc
+    stylelintrc
+    Xresources
+    moc
+    thunderbird
+    gtkrc-2.0
+    CPPLINT.cfg
+    )
 
-for link in "${links[@]}"; do
-    if [[ -e "$HOME/.config/$link" ]]; then
-        echo "Already exists: $HOME/.config/$link"
-        continue
+    for link in "${links[@]}"; do
+        local from="$HOME/.config/$link"
+        local to="$HOME/.$link"
+
+        if [[ ! -e "$from" ]]; then
+            echo "NO CONFIG: $from"
+            continue
+        fi
+
+        if [[ -L "$to" ]]; then
+            if [[ ! -e "$to" ]]; then
+                echo "FIXING BROKEN LINK: $to"
+                rm "$to"
+            else
+                continue
+            fi
+        else
+            if [[ -f "$to" ]]; then
+                echo "CONFIG EXISTS: $to"
+                rm -iv "$to"
+            fi
+        fi
+
+        ln -sv "$from" "$to"
+    done
+
+    if [[ -e "$HOME/.CPPLINT.cfg" ]]; then
+        mv -v "$HOME/.CPPLINT.cfg" "$HOME/CPPLINT.cfg"
     fi
 
-    if [[ ! -e "$HOME/.config/$link" ]]; then
-        echo "BROKEN LINK: $link"
-        continue
+    if ! ls $HOME/.config/mozilla/firefox/*/chrome &> /dev/null; then
+        ln -sv ~/.config/mozilla-chrome-tweaks ~/.config/mozilla/firefox/*/chrome
+    fi
+}
+
+setup_services() {
+    if [[ ! -f /etc/systemd/system/lock-on-suspend@.service ]]; then
+        echo
+        echo "sed \"s/%USER%/$USER/\" ~/.config/lock-on-suspend@.service | sudo tee /etc/systemd/system/lock-on-suspend@.service > /dev/null"
+        echo "sudo systemctl enable lock-on-suspend@$USER"
     fi
 
-	ln -sv "$HOME/.config/$link" "$HOME/.$link"
-done
+    if [[ ! -f /etc/X11/xorg.conf.d/00-keyboard.conf ]]; then
+        echo
+        echo 'sudo cp ~/.config/00-keyboard.conf /etc/X11/xorg.conf.d/'
+    fi
 
-if ls $HOME/.config/mozilla/firefox/*/chrome &> /dev/null; then
-    echo "Already exists: $HOME/.config/mozilla/firefox/*/chrome"
-else
-    ln -sv ~/.config/mozilla-chrome-tweaks ~/.config/mozilla/firefox/*/chrome
-fi
+    if [[ ! -f /etc/modprobe.d/pcspkr-blacklist.conf ]]; then
+        echo
+        echo 'sudo cp ~/.config/pcspkr-blacklist.conf /etc/modprobe.d/'
+    fi
 
-if [[ -e "$HOME/CPPLINT.cfg" ]]; then
-    echo "Already exists: $HOME/CPPLINT.cfg"
-else
-    ln -sv ~/.config/CPPLINT.cfg ~/CPPLINT.cfg
-fi
+    git config --global pull.rebase true
+    git config --global alias.review 'push origin @:refs/for/master'
+    git config --global alias.s 'status'
+    git config --global color.ui auto
+    git config --global core.excludesfile ~/.gitignore
 
-echo
-echo "sed \"s/%USER%/$USER/\" ~/.config/lock-on-suspend@.service | sudo tee /etc/systemd/system/lock-on-suspend@.service > /dev/null"
-echo "sudo systemctl enable lock-on-suspend@$USER"
-echo
-echo 'sudo cp ~/.config/00-keyboard.conf /X11/xorg.conf.d/'
-echo
-echo 'sudo cp ~/.config/pcspkr-blacklist.conf /etc/modprobe.d/'
+    if ! grep -q '\[user\]' "$HOME/.gitconfig"; then
+        echo
+        echo 'Don'\''t forget to add git user'
+    fi
+}
 
-git config --global pull.rebase true
-git config --global alias.review 'push origin @:refs/for/master'
-git config --global alias.s 'status'
-git config --global color.ui auto
-git config --global core.excludesfile ~/.gitignore
-echo
-echo 'Don'\''t forget to add git user'
+setup_font_size() {
+    if [[ "$1" == '--big' ]]; then
+        readonly fontsize=18
+    else
+        readonly fontsize=12
+    fi
+
+    local -r dpi_configs="$HOME/.config/dpi-dependent"
+    cd "$dpi_configs" || exit
+    #TODO: add mozilla CSS as well
+    local configs=( $(find -type f) )
+
+    for config in "${configs[@]}"; do
+        local from="${config#.}"
+        local to="$HOME/.config$from"
+
+        if [[ -e "$to" ]]; then
+            echo "OVERWRITING: $to"
+        fi
+
+        local dir
+        dir="$(dirname "$to")"
+        if [[ ! -d "$dir" ]]; then
+            mkdir -v "$dir"
+        fi
+
+        sed "s/%FONT_SIZE%/$fontsize/" "$dpi_configs$from" > "$to"
+    done
+}
+
+setup_symlinks
+setup_services
+setup_font_size "$1"
