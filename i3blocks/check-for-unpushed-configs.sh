011120099b9fed3afdd9588af240113c3821a2aa
Kostia Fursenko <spiritofsands@protonmail.com>
2019-01-06 10:48:00 +0200

    Update config check

--- i3blocks/check-for-unpushed-configs.sh
+++ i3blocks/check-for-unpushed-configs.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 
-unpushed_config=0
-
+config_flag=0
 cd ~/.config || exit
 if [[ -n $(git status -s) ]]; then
-    unpushed_config=1
+    config_flag=1
 fi
 
-unpushed_bin=0
+config_updates=0
+if git status | grep -q 'is behind'; then
+    config_flag=1
+    config_updates=1
+fi
+
+bin_flag=0
 cd ~/bin || exit
 if [[ -n $(git status -s) ]]; then
-    unpushed_bin=1
+    bin_flag=1
 fi
 
-if [[ "$unpushed_config" -eq 1 || "$unpushed_bin" -eq 1 ]]; then
-    if [[ "$unpushed_config" -eq 1 ]]; then
+bin_updates=0
+if git status | grep -q 'is behind'; then
+    bin_flag=1
+    bin_updates=1
+fi
+
+if [[ "$config_flag" -eq 1 || "$bin_flag" -eq 1 ]]; then
+    if [[ "$config_flag" -eq 1 ]]; then
         echo -n 'config'
+        if [[ "$config_updates" -eq 1 ]]; then
+            echo -n '*'
+        fi
     fi
 
-    if [[ "$unpushed_bin" -eq 1 ]]; then
-        if [[ "$unpushed_config" -eq 1 ]]; then
+    if [[ "$bin_flag" -eq 1 ]]; then
+        if [[ "$config_flag" -eq 1 ]]; then
             echo -n ', '
         fi
 
         echo -n 'bin'
+        if [[ "$bin_updates" -eq 1 ]]; then
+            echo -n '*'
+        fi
     fi
 
     echo
