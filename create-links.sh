713494d08912d4a5376f5481c6ff0289857ba9b0
Kostia Fursenko <spiritofsands@protonmail.com>
2019-01-17 22:12:29 +0200

    Add autologin

--- create-links.sh
+++ create-links.sh
@@ -60,31 +60,42 @@ setup_symlinks() {
         mv -v "$HOME/.CPPLINT.cfg" "$HOME/CPPLINT.cfg"
     fi
 
-    if ! ls $HOME/.config/mozilla/firefox/*/chrome &> /dev/null; then
+    if ! ls "$HOME/.config/mozilla/firefox/"*/chrome &> /dev/null; then
         ln -sv ~/.config/mozilla-chrome-tweaks ~/.config/mozilla/firefox/*/chrome
     fi
 }
 
 setup_services() {
-    if [[ ! -f /etc/systemd/system/lock-on-suspend@.service ]]; then
+    local -r lock_config='/etc/systemd/system/lock-on-suspend@.service'
+    if [[ ! -f "$lock_config" ]]; then
         echo
-        echo "sed \"s/%USER%/$USER/\" ~/.config/lock-on-suspend@.service | sudo tee /etc/systemd/system/lock-on-suspend@.service > /dev/null"
+        echo "sed \"s/%USER%/$USER/\" ~/.config/lock-on-suspend@.service | sudo tee $lock_config > /dev/null"
         echo "sudo systemctl enable lock-on-suspend@$USER"
     fi
 
-    if [[ ! -f /etc/X11/xorg.conf.d/00-keyboard.conf ]]; then
+    local -r keyboard_config='/etc/X11/xorg.conf.d/00-keyboard.conf'
+    if [[ ! -f "$keyboard_config" ]]; then
         echo
-        echo 'sudo cp ~/.config/00-keyboard.conf /etc/X11/xorg.conf.d/'
+        echo "sudo cp ~/.config/00-keyboard.conf $keyboard_config"
     fi
 
-    if [[ ! -f /etc/modprobe.d/pcspkr-blacklist.conf ]]; then
+    local -r pcspkr_config='/etc/modprobe.d/pcspkr-blacklist.conf'
+    if [[ ! -f "$pcspkr_config" ]]; then
         echo
-        echo 'sudo cp ~/.config/pcspkr-blacklist.conf /etc/modprobe.d/'
+        echo "sudo cp ~/.config/pcspkr-blacklist.conf $pcspkr_config"
     fi
 
-    local sysctl_config='/etc/sysctl.d/99-sysctl.conf'
+    local -r sysctl_config='/etc/sysctl.d/99-sysctl.conf'
     if ! grep -q '^kernel.sysrq=502' "$sysctl_config"; then
+        echo
         echo "echo \"kernel.sysrq=502\" | sudo tee $sysctl_config"
+    fi
+
+    local -r lightdm_config='/etc/lightdm/lightdm.conf'
+    if ! grep -q "autologin-user = $USER" "$lightdm_config"; then
+        echo
+        echo "echo \"autologin-user = $USER\" | sudo tee $lightdm_config"
+        echo "echo \"autologin-user-timeout = 0\" | sudo tee $lightdm_config"
     fi
 
     git config --global pull.rebase true
