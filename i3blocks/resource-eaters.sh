402283982999726894b956f1507994861a0085a9
Kostia Fursenko <spiritofsands@protonmail.com>
2019-01-18 10:12:49 +0200

    Improve resourceeaters

--- i3blocks/resource-eaters.sh
+++ i3blocks/resource-eaters.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+readonly resource="${1?}"
+readonly high_level=50
+
 print_name() {
     local -r max_len=10
     local -r max_len_no_braces="$((max_len - 2))"
@@ -13,14 +16,12 @@ print_name() {
     echo "[$name]"
 }
 
-readonly cpu_ps="$(ps -ew --format %cpu=,comm= --sort=-%cpu | head -n 1)"
-cpu_percent="${cpu_ps# *}"
-cpu_percent="${cpu_percent%% *}"
-readonly cpu_name="${cpu_ps##* }"
+readonly resource_ps="$(ps -ew --format "%$resource=,comm=" "--sort=-%$resource" | head -n 1)"
+resource_percent="${resource_ps# *}"
+resource_percent="${resource_percent%% *}"
+resource_name="${resource_ps#* }"
+resource_name="${resource_name#* }"
 
-readonly mem_ps="$(ps -ew --format %mem=,comm= --sort=-%mem | head -n 1)"
-mem_percent="${mem_ps# *}"
-mem_percent="${mem_percent%% *}"
-readonly mem_name="${mem_ps##* }"
-
-echo "$(print_name "$mem_name") $cpu_percent% / $(print_name "$cpu_name") $mem_percent%"
+if [[ "${resource_percent%.*}" -ge "$high_level" ]]; then
+    echo "${resource^^} $(print_name "$resource_name") $resource_percent%"
+fi
